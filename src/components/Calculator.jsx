76854b58b1890fd05fdc84227cb2d35f0de7a9a1
dmwambua <dickmuoki@gmail.com>
2025-03-08 11:24:57 -0500

    added a;; fucntionality to calculator

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,20 +1,65 @@
-import React from "react";
-export default function Calculator() {
+import React, { useState } from "react";
+import { evaluate } from "mathjs";
 const buttons = [
-  "7", "8", "9", "/",
-  "4", "5", "6", "*",
-  "1", "2", "3", "-",
-  "0", ".", "=", "+",
+  "7",
+  "8",
+  "9",
+  "/",
+  "4",
+  "5",
+  "6",
+  "*",
+  "1",
+  "2",
+  "3",
+  "-",
+  "0",
+  ".",
+  "=",
+  "+",
   "C"
 ];
+export default function Calculator() {
+  const [input, setInput] = useState("");
+  function handleClick(value) {
+    if (value === "C") {
+      setInput("");
+    } else if (value === "=") {
+      //run math operation
+      try {
+        const result = evaluate(input).toString();
+        setInput(result);
+      } catch (error) {
+        setInput("Error. Try again");
+      }
+    } else {
+      setInput(prevValue => prevValue + value);
+    }
+  }
   return (
     <div className="calculator">
-      <div className="display">0</div>
+      <div className="display">
+        {input || "Awesome Calc"}
+      </div>
       <div className="buttons">
-        {buttons.map((button,index) => {
-            return <button key ={index}>{button}</button>;
-          })}
+        {buttons.map((value, index) => {
+          return (
+            <button
+              key={index}
+              className={
+                value === "C"
+                  ? "clear"
+                  : value === "="
+                    ? "equals"
+                    : ["/", "*", "-", "+"].includes(value) ? "operator" : ""
+              }
+              onClick={() => handleClick(value)}
+            >
+              {value}
+            </button>
+          );
+        })}
       </div>
-      </div>
-      );
+    </div>
+  );
 }
